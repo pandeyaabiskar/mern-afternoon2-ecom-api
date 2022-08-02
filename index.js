39b1eaf584f3eed99ce842a20a5327ca64857ead
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-08-02 16:49:27 +0545

    Serving static files

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const productRouter = require('./routes/products')
+const productData = require('./data/products.json')
 const app = express();
 
 //Setup our server
 app.use(cors());
 app.set('view engine', 'hbs');
+app.set('views', './templates')
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
-    res.send("Welcome to Ecommerce API");
+    //Passing product data into the hbs file
+    res.render('index', { productData }  );
 })
 
 app.use('/api/products', productRouter)
