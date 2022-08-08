f69b5b9d8542e63bfe8203c8d857c096a1089f67
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-08-08 15:27:54 +0545

    Detail page added

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const productRouter = require('./routes/products')
-const productData = require('./data/products.json')
+const express = require("express");
+const cors = require("cors");
+const productRouter = require("./routes/products");
+const productData = require("./data/products.json");
 const app = express();
 
 //Setup our server
 app.use(cors());
-app.set('view engine', 'hbs');
-app.set('views', './templates')
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", "./templates");
+app.use(express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  //Passing product data into the hbs file
+  res.render("index", { productData });
+});
 
-app.get('/', (req, res) => {
-    //Passing product data into the hbs file
-    res.render('index', { productData }  );
-})
+app.get("/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const selectedProduct = productData.find(
+    (product) => product.id === parseInt(productID)
+  );
+  res.render("details", { product: selectedProduct });
+});
 
-app.use('/api/products', productRouter)
+app.use("/api/products", productRouter);
 
 app.listen(4000, () => {
-    console.log("Server started at port 4000");
-})
+  console.log("Server started at port 4000");
+});
